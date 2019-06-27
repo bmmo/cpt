bda39b7fcfda33ad0cece1e81eb1fa11bb040b1e
Bruno Oliveira <bruno.oliveira@leicabiosystems.com>
2019-06-27 16:56:44 +0100

    Comissao inicial

---
+++ include/pt.h
@@ -0,0 +1,15 @@
+#pragma once
+
+// A linguagem C
+#define define   define
+#define inclui   include
+#define se       if
+#define senao    else
+#define enquanto while
+#define continua continue
+#define faz      do
+
+
+// Multi-fios de execucao
+#define fio            thread
+#define exclusao_mutua mutex
